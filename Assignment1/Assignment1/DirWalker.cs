905b47c6abd3da9e0e79c0ed6d1a4b2857b19e15
A00456453 <Javal.Rajesh.Vadodaria@smu.ca>
2021-10-24 21:13:27 -0300

    Assignment 1

---
+++ Assignment1/Assignment1/DirWalker.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+
+namespace Assignment1
+{
+  
+
+    public class DirWalker
+    {
+
+        public void walk(String path, ref string content,ref int errorcount, ref int normalcount)
+        {
+
+            string[] list = Directory.GetDirectories(path);
+
+            if (list == null) return;
+
+            foreach (string dirpath in list)
+            {
+                if (Directory.Exists(dirpath))
+                {
+                    walk(dirpath, ref content,ref errorcount,ref normalcount);
+                   // Console.WriteLine("Dir:" + dirpath);
+                }
+            }
+            string[] fileList = Directory.GetFiles(path,"*.csv");
+            foreach (string filepath in fileList)
+            {
+                SimpleCSVParser csv = new SimpleCSVParser();
+                csv.parse(filepath, ref content, ref errorcount, ref normalcount);
+                    //Console.WriteLine("File:" + filepath);
+            }
+            File.AppendAllText("E:\\SoftwareClass\\output\\result\\.csv", content);
+            content = "";
+        }
+
+        public static void Main(String[] args)
+        {
+
+            string content = "";
+            int errorcount = 0;int normalcount = 0;
+            File.WriteAllText("E:\\SoftwareClass\\output\\result.csv", "");
+
+
+            var watch = new System.Diagnostics.Stopwatch();
+
+            watch.Start();
+
+            
+
+            watch.Stop();
+
+           long time = watch.ElapsedMilliseconds;
+
+            DirWalker fw = new DirWalker();
+            fw.walk("E:\\SoftwareClass\\Sample Data", ref content, ref errorcount,ref normalcount);
+
+            File.AppendAllText("E:\\SoftwareClass\\output\\result.csv", "total skipped records-" + errorcount + " total records-" + normalcount + " time taken ms:" + time);
+            
+        }
+
+    }
+}
---
+++ Assignment1/Assignment1/SimpleCSVParser.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualBasic.FileIO;
+
+namespace Assignment1
+{
+    public class SimpleCSVParser
+    {
+
+
+        //       public static void Main(String[] args)
+        //       {
+        //           SimpleCSVParser parser = new SimpleCSVParser();
+        //           parser.parse(@"/Users/dpenny/Projects/Assignment1/Assignment1/sampleFile.csv");
+        //       }
+
+
+        public void parse(String fileName, ref string content, ref int Skipped, ref int normalcount)
+        {
+            try
+            {
+                //int Header = 0;
+                int i = 0;
+                //int Skipped = 0;
+                string result="";
+                using (TextFieldParser parser = new TextFieldParser(fileName))
+                {
+                    i = 0;
+                    parser.TextFieldType = FieldType.Delimited;
+                    parser.SetDelimiters(",");
+
+                    while (!parser.EndOfData)
+                    {
+                        i = 0;
+                        result = "";
+                        //Process row
+                        string[] fields = parser.ReadFields();
+                        foreach (string field in fields)
+                        {
+                            if (field!="")
+                            {
+                                result +=  field + ",";
+                            }
+                            else
+                            {
+                                i = 1;
+                                break;
+                            }
+                            //Console.WriteLine(field);
+                        }
+
+                        if (i != 1)
+                        {
+                            content += result;
+                            content = content.Substring(0, content.Length - 1);
+                            content += "\r\n";
+                            normalcount++;
+                        }
+                        else
+                            Skipped++;
+                    }
+                }
+
+            }
+            catch (IOException ioe)
+            {
+                Console.WriteLine(ioe.StackTrace);
+            }
+
+        }
+
+
+    }
+}
